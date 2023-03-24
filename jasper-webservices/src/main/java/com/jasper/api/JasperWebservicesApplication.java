390416f7a760e8edf0b3d383d004826189d9192d
jaoearn <sutheera.p@appman.co.th>
2023-03-24 13:41:10 +0700

    SpringBootApplication

--- jasper-webservices/src/main/java/com/jasper/api/JasperWebservicesApplication.java
+++ jasper-webservices/src/main/java/com/jasper/api/JasperWebservicesApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JasperWebservicesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JasperWebservicesApplication.class, args);
 	}
-
 }
